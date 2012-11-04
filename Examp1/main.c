e29eb710f2877da6bde909eb77c796c65a040597
Rostam Shirani <rostamshirani@RostamMacBook-3.local>
2012-11-04 14:13:26 -0500

    Initial Commit

---
+++ Examp1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Examp1
+//
+//  Created by Rostam Shirani on 12-11-04.
+//  Copyright (c) 2012 Carleton University. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
